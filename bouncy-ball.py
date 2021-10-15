4016f322b1e8c321b32302268b3df17626b096bb
山主 <shanzhu@raysengine.com>
2021-10-15 15:57:21 +0800

    Add basic velocity mechanism
    Add some new mechanism. Currently support:
    1. Velocity system.
    2. Plural bouncy balls exist on screen simultaneously.
    
    Modify some code structure:
    1. Unified update/render function which can update/render a bat-
       ch of bouncy ball.
    2. Primitive collision detection with window border is implemen-
       ted and put in bouncy ball update function to avoid extra ca-
       lling for collision detection.
    
    Signed-off-by: 山主 <shanzhu@raysengine.com>

--- bouncy-ball.py
+++ bouncy-ball.py
@@ -2,14 +2,21 @@ import tkinter as tk
 import time    as tm
 
 
-GRAVITY = 9.8
+# Global Variables
+SCALE = 30.0
+GRAVITY = 9.8 * SCALE
+CANVAS = None
+CANVAS_HEIGHT = 600
+CANVAS_WIDTH  = 800
+DELTA_TIME = 0.01
+BOUNCY_BALL_NUM = 5
 
 
+# Structs
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
 
 class Rectangle:
     def __init__(self, x1, y1, x2, y2):
@@ -18,36 +25,47 @@ class Rectangle:
         self.x2 = x2
         self.y2 = y2
 
+        self.left_up      = Point(x1, y1)
+        self.right_bottom = Point(x1, y1)
 
 class Bouncy_Ball:
-    def __init__(self, canvas, radius, x, y, width=1.0):
-        self.canvas = canvas
+    ''' a physic-based bouncy ball '''
+
+    def __init__(self, radius, x, y, x_speed, y_speed, width=1.0):
         self.radius = radius
         self.center = Point(x, y)
+        self.speed  = Point(x_speed, y_speed)
         self.width  = width
 
         self.bounding_box = Rectangle(x - radius, y - radius, x + radius, y + radius)
-        self.id = canvas.create_oval(self.bounding_box.x1, self.bounding_box.y1,
+        self.id = CANVAS.create_oval(self.bounding_box.x1, self.bounding_box.y1,
                                      self.bounding_box.x2, self.bounding_box.y2,
                                      width=width)
 
-        self.update()
-        self.render()
-
-    def move(self, x_diff, y_diff):
-        self.set_center(self.center.x + x_diff, self.center.y + y_diff)
-        self.update()
-        self.render()
-
     def update(self):
-        self.bounding_box= Rectangle(self.center.x - self.radius,
-                                     self.center.y - self.radius,
-                                     self.center.x + self.radius,
-                                     self.center.y + self.radius)
+        delta_speed   = Point(0.0, 0.0)
+        delta_speed.x = 0.0
+        delta_speed.y = GRAVITY * DELTA_TIME
+
+        self.center.x += (self.speed.x + 0.5 * delta_speed.x) * DELTA_TIME
+        self.center.y += (self.speed.y + 0.5 * delta_speed.y) * DELTA_TIME
+        self.speed.x += delta_speed.x
+        self.speed.y += delta_speed.y
+
+        self.bounding_box = Rectangle(self.center.x - self.radius,
+                                      self.center.y - self.radius,
+                                      self.center.x + self.radius,
+                                      self.center.y + self.radius)
+
+        # need change to use force analysis for real physic-based bouncy ball
+        if self.center.x + self.radius > CANVAS_WIDTH or self.center.x - self.radius < 0.0:
+            self.speed.x = -self.speed.x
+        if self.center.y + self.radius > CANVAS_HEIGHT:
+            self.speed.y = -self.speed.y
 
     def render(self):
-        self.canvas.coords(self.id, [self.bounding_box.x1, self.bounding_box.y1,
-                                     self.bounding_box.x2, self.bounding_box.y2])
+        CANVAS.coords(self.id, [self.bounding_box.x1, self.bounding_box.y1,
+                                self.bounding_box.x2, self.bounding_box.y2])
 
     def set_radius(self, radius):
         self.radius = radius
@@ -56,29 +74,51 @@ class Bouncy_Ball:
         self.center.x = x
         self.center.y = y
 
+    def change_radius(self, radius_diff):
+        self.radius += radius_diff
 
+    def change_center(self, x_diff, y_diff):
+        self.center.x += x_diff
+        self.center.y += y_diff
+
+
+# Tools
+def draw_call():
+    ''' draw elements on canvas '''
+    CANVAS.update_idletasks()
+    CANVAS.update()
+
+def update(update_queue):
+    ''' object irrelevant update '''
+    for update_item in update_queue:
+        update_item.update()
+
+def render(render_queue):
+    ''' object irrelevant render '''
+    for render_item in render_queue:
+        render_item.render()
+
+    draw_call()
+
+
+# Entrance
 if __name__ == '__main__':
     board = tk.Tk()
     board.title('bouncy ball')
 
-    canvas = tk.Canvas(board, width=800, height=600, bg='ivory')
-    canvas.pack()
+    CANVAS = tk.Canvas(board, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, bg='ivory')
+    CANVAS.pack()
 
-    bouncy_ball = Bouncy_Ball(canvas, 20, 100, 100)
-
-    x_offset = 0
-    y_offset = 0
-    time_pass = 0.0
+    update_queue = []
+    render_queue = []
+    for i in range(BOUNCY_BALL_NUM):
+        bouncy_ball = Bouncy_Ball(20, 400, 100, -40.0 + i * 20.0, 0.0)
+        update_queue.append(bouncy_ball)
+        render_queue.append(bouncy_ball)
 
     while True:
-        tm.sleep(0.01)
-        time_pass += 0.01
+        tm.sleep(DELTA_TIME)
 
-        y_offset = 0.5 * GRAVITY * time_pass * time_pass
-        bouncy_ball.move(x_offset, y_offset)
-        canvas.update_idletasks()
-        canvas.update()
-
-        if bouncy_ball.center.y > 600:
-            bouncy_ball.center.y = 0
+        update(update_queue)
+        render(render_queue)
 
